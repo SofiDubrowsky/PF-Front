8f4b2567752ad6e65c2a3c20d98722aa05175603
L-J-Sanchez-Zamora <lady-latina1@hotmail.com>
2023-07-02 16:43:54 -0500

    Funcion con Switch

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function laCajaDePandora(numero){
   //proximamente escribiremos codigo aqui
+  
+
+    switch (numero % 2) {
+        case 0:
+            return numero.toString(2);
+        default:
+            return numero.toString(16);
+
+    }
 }
+
