f7b3cdf9c4f04f8b599a71b5456bf8f9c87c5361
nadiamartel <nadiagmartel@gmail.com>
2023-07-02 18:11:49 -0300

    firts commit

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function laCajaDePandora(numero){
     return numero.toString(16);
   }
 }
+
+const nadiaMartel = () =>{
+  const datos = {
+    nombre: "Nadia Martel",
+    nacionalidad: "Argentina",
+    edad: 32
+  };
+  return datos
+}
