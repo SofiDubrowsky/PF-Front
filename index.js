fda4fe3792f467268d7e4cc37b498cd78ff15b1b
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-07-02 18:24:36 -0300

    add new function index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function laCajaDePandora(numero){
   //proximamente escribiremos codigo aqui
+    if(numero % 2 === 0) numero.toString(2)
+
+    return numero.toString(16);
+}
+const lucianoVel = () =>{
+  const datos = {
+    nombre: "Luciano",
+    nacionalidad: "Argentina",
+    edad: 27
+  };
+  return datos
 }
