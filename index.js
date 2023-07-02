cfe312b81dd87995988d4de6212e9db3bc4ed5ef
FabriVettorelo <swfabri@gmail.com>
2023-07-01 23:42:53 -0300

    f CadaDePandora. FV

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function laCajaDePandora(numero){
-  //proximamente escribiremos codigo aqui
+    if (numero % 2 === 0) {
+        // El número es par
+        return numero.toString(2); // Convierte a binario y devuelve el resultado
+      } else {
+        // El número es impar
+        return numero.toString(16); // Convierte a hexadecimal y devuelve el resultado
+      }
+}
+
+const fabriVettorelo = ()=>{
+    const datos = {
+        nombre: "Fabri Vettorelo",
+        edad: 26,
+        nacionalidad: "Argentina" 
+    }
+    return datos
 }
