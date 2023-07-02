4858e42f3b378feed7226e972fd95d3a72465b0b
agustinatahhan <agustahhan@gmail.com>
2023-07-02 18:25:43 -0300

    agus

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function laCajaDePandora(numero){
   //proximamente escribiremos codigo aqui
+    return numero % 2 === 0 ? numero.toString(2) : numero.toString(16);
+}
+
+function agus(){
+    const info = {
+        nombre : "agus",
+        nacionalidad: "argentina",
+        edad: 28
+    }
+    return info;
 }
