a6fb0b75f4d891e76d11535a2b597aa02dea840f
Gonzalo Schmidt <gonza.sch35@gmail.com>
2023-07-02 18:25:50 -0300

    Gonza

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function laCajaDePandora(numero){
   //proximamente escribiremos codigo aqui
+  const parOImpar = numero % 2;
+  if(parOImpar === 0) return numero.toString(2)
+  if(parOImpar !== 0) return numero.toString(16)
 }
