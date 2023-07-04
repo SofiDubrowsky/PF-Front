b9174cebe9f9cbc9e49498bcd8b722336be0eb1e
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-07-04 19:22:25 -0300

    feat: create nav component

---
+++ Client/src/Components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+    return (
+      <div>
+      <NavLink to="">Iniciar Sesion/Registrarse</NavLink>
+      <NavLink to="/">Salir</NavLink>
+      <NavLink to="/post">Crear Actividad</NavLink>
+      <NavLink to="/home">Logo</NavLink>
+      </div>
+    )
+}
+
+export default Nav;
