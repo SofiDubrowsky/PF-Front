fcdade22941fe8e56ffb418ce5368a27c8056e11
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-07-04 20:05:46 -0300

    feat: add links to facebook and instagram. SD

--- Client/src/Components/Footer/Footer.jsx
+++ Client/src/Components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from "react-router-dom";
+import instagram from '../../assets/instagram.png'
+import facebook from '../../assets/facebook-circular-logo.png'
 
 const Footer = () => {
     return (
       <div>
-          <NavLink to="/about">About</NavLink>
           <NavLink to="/contact">Contacto</NavLink>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagram} alt="instagram" />
+          </a> 
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebook} alt="facebook" />
+          </a> 
       </div>
     )
 }
