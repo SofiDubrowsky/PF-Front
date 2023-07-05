a198fe8950b4a0cf72039bff13f32e758e67d05e
Leidy Johanna Sánchez Zamora <124950194+L-J-Sanchez-Zamora@users.noreply.github.com>
2023-07-05 17:11:00 -0500

    feat/home,paging,search,recuder

---
+++ Client/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import getactbyname from '../../actions/getactbyname';
+import stl from './SearchBar.module.css'
+
+export default function SearchBar() {
+    const dispatch = useDispatch()
+    const [name, setName] = useState('')
+
+    function handleinputChange(e) {
+        e.preventDefault()
+        setName(e.target.value)
+    }
+
+   function handleSubmit(e) {
+        e.preventDefault()
+        dispatch(getactbyname(name))
+        setName("")
+    }
+
+    return (
+        <div className={stl.sbcontainer}>
+            <input className={stl.sbinput} onChange={(e) => handleinputChange(e)} type='text' placeholder='Search by name' value={name} />
+            <button className={stl.sbbot} onClick={(e) => handleSubmit(e)} type='submit'>Buscar🔎</button>
+        </div>
+    )
+}
+
+//se debe crear las actions getactbyname y la ruta sugerir que me dejen hacer activities en el back
