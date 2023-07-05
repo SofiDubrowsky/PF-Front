ab1bd048259ed6ea754a15738e3a21e2d702f49c
Juan Pablo Mora <jmoracafe@gmail.com>
2023-07-04 20:30:02 -0600

    feat:Agregue landing view JPM

--- Client/src/Views/Landing/Landing.jsx
+++ Client/src/Views/Landing/Landing.jsx
@@ -1,9 +1,22 @@
-
+import style from './Landing.module.css'
 
 const Landing = () => {
     return (
         <div>
-            <h1>Soy Landing</h1>
+            <nav className={style.navBar}></nav>
+            <div className={style.gridContainer}>
+                <div className={style.textContainer}>
+                    <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, eius ipsa! Accusantium magni quidem ipsam error atque incidunt qui perspiciatis reprehenderit. Quasi alias, numquam eum optio quod doloribus illo voluptatum?</p>
+                    <button className={style.buttonEnter}>Busca tu actividad</button>
+                </div>
+                <img src="https://www.touchtaiwan.com/images/default.jpg" alt="" className={style.principalImg}/>
+                <div className={style.imgContainer}>
+                    <img src="https://www.touchtaiwan.com/images/default.jpg" alt="" className={style.secondaryImg}/>
+                    <img src="https://www.touchtaiwan.com/images/default.jpg" alt="" className={style.secondaryImg}/>
+                    <img src="https://www.touchtaiwan.com/images/default.jpg" alt="" className={style.secondaryImg}/>
+                </div>
+            </div>
+            <footer className={style.footer}></footer>
         </div>
     )
 }
