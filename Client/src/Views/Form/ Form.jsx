f390f82649daa61f7917e42d2e4230278df1ffe4
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-07-05 14:49:14 -0300

    fix: componenten form changes in progress

--- Client/src/Views/Form/ Form.jsx
+++ Client/src/Views/Form/ Form.jsx
@@ -14,11 +14,29 @@ const Form = () => {
 
     return (
         <div>
-            <h1>Soy Form</h1>
+            <h1>Añadir nueva actividad</h1>
             <form >
-            <label>Nombre</label><input type="text" value={form.name} name="name"/>
-            <label>IMG</label><input type="text" value={form.picture} name="picture"/>
-            <label>Horarios</label><input type="text" value={form.hours.join(",")}/>
+            <label>Titulo</label>
+            <input type="text" value={form.name} name="name"/>
+
+            <label>Fotos</label>
+            <input type="text" value={form.picture} name="picture"/>
+
+            <label>Descripción</label>
+            <textarea type="text" value={form.description} name="description"/>
+
+            <label>Dias</label>
+            <input type="text" value={form.days.join(",")}/>
+              <select type="text" value={form.days} name="dias">
+                <option value="lunes">lunes</option>
+                <option value="martes">martes</option>
+                <option value="miercoles">miercoles</option>
+                <option value="jueves">jueves</option>
+                <option value="viernes">viernes</option>
+                <option value="sabado">sabado</option>
+              </select>
+
+            <label>Horarios</label>
               <select type="text" value={form.hours} name="horas">
                 <option value="10-11">10hs a 11hs</option>
                 <option value="11-12">11hs a 12hs</option>
@@ -30,16 +48,29 @@ const Form = () => {
                 <option value="18-19">18hs a 19hs</option>
                 <option value="19-20">19hs a 20hs</option>
               </select>
-            <label>Dias</label><input type="text" value={form.days.join(",")}/>
-              <select type="text" value={form.days} name="dias">
-                <option value="lunes">lunes</option>
-                <option value="martes">martes</option>
-                <option value="miercoles">miercoles</option>
-                <option value="jueves">jueves</option>
-                <option value="viernes">viernes</option>
-                <option value="sabado">sabado</option>
-              </select>
-            <label>Descripcion</label><textarea type="text" value={form.description} name="description"/>
+
+              <div className="hoursConteiner">
+                <div className="hoursSelected">
+                  {
+                  form?.hours?.length > 0 ?
+                  (
+                    form?.hours?.map((hour) => (
+                      <div className="hour">
+                        <h3>{hour}</h3>
+                      </div>
+                      )))
+                  : (<p>No se han seleccionado horarios</p>)  
+                  }
+                </div>
+              </div>
+
+
+              <label>Sucursales</label>
+                <select name='sucursal'>
+                  <option value="" disabled selected>Seleccione Sucursal</option>
+                  <option value="Cerro de las Rosas">Cerro de las Rosas</option>
+                </select>
+
         <button type="submit">Crear</button>
             </form>
         </div>
