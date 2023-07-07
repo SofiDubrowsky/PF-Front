0bd7697dd39a8e46644e6bd5891d078caf77560a
Juan Pablo Mora Del Castillo <120071610+JuanPabloMora61@users.noreply.github.com>
2023-07-07 11:56:43 -0600

    fix/ changed styles in cards and searchbar

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -2,6 +2,8 @@ import style from "./Card.module.css";
 import { NavLink } from "react-router-dom";
 
 const Card = ({ name, id, picture, stores, cost, players }) => {
+  const im = picture.replace("{", "").replace("}", "")
+  const images = im.split(",")
   return (
     <NavLink to={`/detail/${id}`} className={style.navlink}>
       <div className={style.container}>
@@ -13,10 +15,10 @@ const Card = ({ name, id, picture, stores, cost, players }) => {
             <h3>{name}</h3>
           </div>
           <div className={style.img}>
-            <img src={picture} alt={name} className={style.cardImg} />
+          <img src={images[0]} alt={name} className={style.cardImg} />
           </div>
           <div className={style.content}>
-            <h5>${cost}</h5>
+            <h5>${cost} /hora</h5>
             <h5>Jugadores: {players.map((player) => player)}</h5>
           </div>
         </div>
