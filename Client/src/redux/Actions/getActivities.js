14345a42548f1fafb4ce2f003e15f2a7dfa2ed8a
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-07-06 22:55:38 -0300

    fix: component form updated, styles required.SD

--- Client/src/redux/Actions/getActivities.js
+++ Client/src/redux/Actions/getActivities.js
@@ -5,10 +5,6 @@ export const GET_ACTIVITIES = 'GET_ACTIVITIES';
 export function getActivities() {
     return async function (dispatch) {
         var result = await axios.get('http://localhost:3001/activities');
-<<<<<<< Updated upstream
-        console.log(result)
-=======
->>>>>>> Stashed changes
         return dispatch({
             type: GET_ACTIVITIES,
             payload: result.data
