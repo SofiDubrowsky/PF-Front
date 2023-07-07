0f84946d682d305a92a9adcb91c063c59546f5d5
Juan Pablo Mora Del Castillo <120071610+JuanPabloMora61@users.noreply.github.com>
2023-07-07 17:50:47 -0600

    fix/filters now keeps the info

--- Client/src/redux/Actions/filters.js
+++ Client/src/redux/Actions/filters.js
@@ -3,7 +3,14 @@
 
 export const ORDER_BY_COST = "ORDER_BY_COST";
 export const ALL_FILTER = "ALL_FILTER";
+export const SET_FILTERS = "SET_FILTERS"
 
+export const setFilters = (payload) => {
+  return {
+    type: SET_FILTERS,
+    payload,
+  };
+}
 
 export const orderCost = (payload) => {
   return {
