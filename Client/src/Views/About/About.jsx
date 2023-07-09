41065d114c8405a44e04943a9261ea9a84788a53
nadiamartel <nadiagmartel@gmail.com>
2023-07-09 02:56:13 -0300

    feat:css_about

--- Client/src/Views/About/About.jsx
+++ Client/src/Views/About/About.jsx
@@ -3,11 +3,13 @@ import style from "./About.module.css";
 import img1 from "../../assets/images/paintball.png";
 import img2 from "../../assets/images/futbol.png";
 import img3 from "../../assets/images/paddel.png";
+// import mision from "../../assets/mision.png";
+// import vision from "../../assets/vision.png";
 
 const About = () => {
-  const [currentImage, setCurrentImage] = useState(img1); 
-  const [imageIndex, setImageIndex] = useState(0); 
-  const images = [img1, img2, img3]; 
+  const [currentImage, setCurrentImage] = useState(img1);
+  const [imageIndex, setImageIndex] = useState(0);
+  const images = [img1, img2, img3];
 
 
   const changeImage = (image) => {
@@ -30,8 +32,8 @@ const About = () => {
 
   return (
     <div>
-      <div className={style.title}>
-        <h1>Sobre nosotros</h1>
+      <div>
+        <h1 className={style.ab}>Nuestro proposito❤</h1>
       </div>
 
       <div className={style.aboutContainer}>
@@ -40,7 +42,7 @@ const About = () => {
             <img
               src={currentImage}
               alt="Imagen grande"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "17px" }}
             />
           </div>
           <div className={style.minimage}>
@@ -56,37 +58,31 @@ const About = () => {
           </div>
         </div>
         <div className={style.description}>
-          <div>
-            <h1>Misión:</h1>
-            <br />
-            <p>
-              En el complejo deportivo Sportiverse, nuestra misión es crear un
-              espacio acogedor y amigable que promueva el bienestar y la diversión
-              para nuestros usuarios y sus familias. Nos esforzamos por ofrecer
-              servicios de calidad en actividades sociales y deportivas, fomentando
-              la convivencia, la salud y el entretenimiento. Nuestro compromiso se
-              basa en la integridad y respeto, buscando brindar a nuestros usuarios
-              una experiencia cálida y gratificante.
-            </p>
-            <br />
-          </div>
-          <div>
-            <h1>Visión:</h1>
-            <br />
-            <p>
-              Nos visualizamos como un lugar excepcionalmente acogedor, donde el
-              bienestar y la diversión sean los pilares fundamentales. Buscamos
-              convertirnos en un referente a nivel local e nacional en la creación
-              de un entorno que promueva el bienestar físico, emocional y social de
-              nuestros usuarios y sus familias. A través de la excelencia en nuestros
-              servicios, instalaciones modernas y accesibles, nos esforzamos por ser
-              reconocidos como un complejo deportivo y social que inspire un estilo
-              de vida saludable y lleno de alegría, donde cada visita se convierta
-              en una experiencia memorable.
-            </p>
-            <br />
-          </div>
+          <h1 className={style.h1}>Misión:</h1>
+          <p className={style.text}>
+            En el complejo deportivo Sportiverse, nuestra misión es crear un
+            espacio acogedor y amigable que promueva el bienestar y la diversión
+            para nuestros usuarios y sus familias. Nos esforzamos por ofrecer
+            servicios de calidad en actividades sociales y deportivas, fomentando
+            la convivencia, la salud y el entretenimiento. Nuestro compromiso se
+            basa en la integridad y respeto, buscando brindar a nuestros usuarios
+            una experiencia cálida y gratificante.
+          </p>
+          <hr />
+          <h1 className={style.h1}>Visión:</h1>
+          <p className={style.text}>
+            Nos visualizamos como un lugar excepcionalmente acogedor, donde el
+            bienestar y la diversión sean los pilares fundamentales. Buscamos
+            convertirnos en un referente a nivel local e nacional en la creación
+            de un entorno que promueva el bienestar físico, emocional y social de
+            nuestros usuarios y sus familias. A través de la excelencia en nuestros
+            servicios, instalaciones modernas y accesibles, nos esforzamos por ser
+            reconocidos como un complejo deportivo y social que inspire un estilo
+            de vida saludable y lleno de alegría, donde cada visita se convierta
+            en una experiencia memorable.
+          </p>
         </div>
+
       </div>
     </div>
   );
