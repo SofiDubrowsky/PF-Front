9e947a7742cb22ebb8219ff1c3bf7b9edb65742f
FabriVettorelo <swfabri@gmail.com>
2023-07-19 13:18:28 -0300

    update user ready

---
+++ Client/src/redux/Actions/updateUser.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export const PUT_USER = "PUT_USER";
+
+export const updateUser = (user) => {
+    return async (dispatch) => {
+        console.log(user)
+        const id = localStorage.getItem('clientId');
+    const info = await axios.put(`https://sportiverse-server.onrender.com/users/${id}`, user);
+       // const info = await axios.put(`http://localhost:3001/users/${id}`, user);
+        
+        return dispatch({ type: PUT_USER, payload: info.data });
+
+    }
+}
