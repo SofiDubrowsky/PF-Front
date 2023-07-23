c0664d8def136aba84c13431748a600c830a98a3
agustinatahhan <agustahhan@gmail.com>
2023-07-23 19:21:55 -0300

    feat/actividades-dashboard

--- Client/src/redux/Actions/getActivityDetail.js
+++ Client/src/redux/Actions/getActivityDetail.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-export const GET_ACTIVITY_DETAIL = "GET_ACTIVITY_DETAIL"
+export const GET_ACTIVITY_DETAIL = "GET_ACTIVITY_DETAIL";
 
-export const getActivityDetail=(id)=>{
-    return async(dispatch)=>{
-         let info = await axios.get(`http://localhost:3001/activities/${id}`);
-        // let info = await axios.get(`https://sportiverse-server.onrender.com/activities/${id}`);
-        return dispatch({type:GET_ACTIVITY_DETAIL,payload:info.data})
-    }
-}
+export const getActivityDetail = (id) => {
+  return async (dispatch) => {
+    let info = await axios.get(`http://localhost:3001/activities/${id}`);
+    // let info = await axios.get(`https://sportiverse-server.onrender.com/activities/${id}`);
+    return dispatch({ type: GET_ACTIVITY_DETAIL, payload: info.data });
+  };
+};
