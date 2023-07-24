a63541a6ab9d15f0fffae7cfcb89c2b3b6ec70a1
agustinatahhan <agustahhan@gmail.com>
2023-07-24 15:51:31 -0300

    eibout

---
+++ Client/src/Views/AboutUs/AboutUs.jsx
@@ -0,0 +1,281 @@
+import style from "./AboutUs.module.css";
+import imagen1 from "../../assets/images/agustina.jpg";
+import imagen2 from "../../assets/images/fabri.jpg";
+import imagen3 from "../../assets/images/gonzalo.png";
+import imagen4 from "../../assets/images/juan.jpg";
+import imagen5 from "../../assets/images/sofi.jpg";
+import imagen6 from "../../assets/images/nadia.jpg";
+import imagen7 from "../../assets/images/luciano.jpg";
+import imagen8 from "../../assets/images/descarga.jfif"
+
+const AboutUs = () => {
+  const github = {
+    juan: "https://github.com/JuanPabloMora61",
+    fabri: "https://github.com/FabriVettorelo",
+    sofi: "https://github.com/SofiDubrowsky",
+    agus: "https://github.com/agustinatahhan",
+    nadia: "https://github.com/nadiamartel",
+    gonza: "https://github.com/Gonzasch35",
+    lucho: "https://github.com/LucianoVelasquez",
+  };
+
+  const linkedin = {
+    juan: "https://www.linkedin.com/in/juanpablomora61",
+    fabri: "https://www.linkedin.com/in/fabrizio-vettorelo-0629a3263/",
+    sofi: "https://www.linkedin.com/in/disof%C3%ADadubrowsky",
+    agus: "https://www.linkedin.com/in/agustinatahhan/",
+    nadia: "https://www.linkedin.com/in/nadia-martel-admin/",
+    gonza: "https://www.linkedin.com/in/gonzalo-schmidt-240903250/",
+    lucho: "https://www.linkedin.com/in/luciano-velasquez-a73252269/",
+  };
+
+  return (
+    <div className={style.container}>
+        <h2>Nuestro Equipo</h2>
+      <div className={style.grid}>
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen4} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Juan Pablo Mora</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.juan} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={linkedin.juan} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn} ${style.linkedin}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen2} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Fabricio Vettorelo</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.fabri} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={linkedin.fabri} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn} ${style.linkedin}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen1} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Agustina Tahhan</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.agus} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={linkedin.agus} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn} ${style.linkedin}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen5} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Sofía Dubrowsky</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.sofi} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={linkedin.sofi} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn} ${style.linkedin}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen6} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Nadia Martel</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.nadia} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={github.nadia} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn} ${style.linkedin}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen3} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Gonzalo Schmidt</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.gonza} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={linkedin.gonza} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn} ${style.linkedin}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardPhoto}>
+            <img src={imagen7} />
+          </div>
+          <div className={style.cardTitle}>
+            <h4>Luciano Velasquez</h4>
+          </div>
+          <div className={style.cardSocials}>
+            <a href={github.lucho} target="_blank" rel="noopener noreferrer">
+              <button className={style.cardSocialsBtn}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="33"
+                  height="33"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </button>
+            </a>
+            <a href={linkedin.lucho} target="_blank" rel="noopener noreferrer">
+              <button className={`${style.cardSocialsBtn}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="512"
+                  viewBox="0 0 512 512"
+                  height="512"
+                >
+                  <path d="m51.326 185.85h90.011v270.872h-90.011zm45.608-130.572c-30.807 0-50.934 20.225-50.934 46.771 0 26 19.538 46.813 49.756 46.813h.574c31.396 0 50.948-20.814 50.948-46.813-.589-26.546-19.551-46.771-50.344-46.771zm265.405 124.209c-47.779 0-69.184 26.28-81.125 44.71v-38.347h-90.038c1.192 25.411 0 270.872 0 270.872h90.038v-151.274c0-8.102.589-16.174 2.958-21.978 6.519-16.174 21.333-32.923 46.182-32.923 32.602 0 45.622 24.851 45.622 61.248v144.926h90.024v-155.323c0-83.199-44.402-121.911-103.661-121.911z"></path>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
